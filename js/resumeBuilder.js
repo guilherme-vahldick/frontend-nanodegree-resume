96b1e5efe5a8aed75e3747d303e3b3962028b21c
guilherme-vahldick <guilhermevahldick@gmail.com>
2015-09-02 12:08:24 -0300

    Commi Test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*
+$('#main').append('Hello Again!');
+var awesomeThoughts = "Im AWESOME";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$('#main').append(funThoughts);
+*/
+
+var formattedName = HTMLheaderName.replace("%data%", "Guilherme Vahldick"),
+    formattedRole = HTMLheaderRole.replace("%data%", "Web Designer");
+
+$('#header').append(formattedName + formattedRole);
